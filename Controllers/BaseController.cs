fee852d65dcb98523ce4ae7b17c46c0ff3cf4886
Thor181 <art.torgaschin@yandex.ru>
2023-10-18 21:50:43 +0700

    Добавьте файлы проекта.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireConstructor.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public abstract string ViewPath { get; }
+
+
+    }
+}
---
+++ Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionnaireConstructor.Controllers
+{
+    public class MainController : BaseController
+    {
+        public override string ViewPath => "~/Views/MainView.cshtml";
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(ViewPath);
+        }
+    }
+}
