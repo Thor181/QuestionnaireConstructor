03a5413698b32b2e2bef7a884ea6a4d2f0cde637
Thor181 <art.torgaschin@yandex.ru>
2023-10-21 01:04:48 +0700

    .

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿//@ts-check
+var gulp = require("gulp");
+
+const concat = require('gulp-concat');
+
+const src = ['wwwroot/css/blocks/**', 'wwwroot/css/modifiers/**', 'wwwroot/css/shared/consts.css', 'wwwroot/css/shared/site.css',];
+
+exports.default = function () {
+    // All events will be watched
+    gulp.watch(src, { events: 'all',ignoreInitial: false }, function (cb) {
+        gulp.src(src).pipe(concat('bundle.css')).pipe(gulp.dest('wwwroot/css/shared'));
+        cb();
+    });
+};
