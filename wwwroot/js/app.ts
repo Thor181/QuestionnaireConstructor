0162cbe8cab2d7fa228a8f61744f0911b4362de9
Thor181 <art.torgaschin@yandex.ru>
2023-11-13 22:08:18 +0700

    feat: up commit for Multiselect slide

--- wwwroot/js/app.ts
+++ wwwroot/js/app.ts
@@ -1,4 +1,5 @@
-﻿import AvailableSlide from './components/AvailableSlide.js';
+﻿import { availableParallelism } from 'os';
+import AvailableSlide from './components/AvailableSlide.js';
 import SidebarItem from './components/SidebarItem.js'
 import { GlobalMeta } from './shared/GlobalMeta.js';
 
@@ -22,12 +23,23 @@ $(async function () {
     questionAvailableSlide.rendered.title = 'Question slide';
     questionAvailableSlide.rendered.imageModifier = 'slide-wrapper__thumbnail-picture--question';
     questionAvailableSlide.rendered.schemeName = 'question';
-    questionAvailableSlide.rendered.schemeContent = '{ "Title":"", "Subtitle":"", "Question":"", "Assistive text":"", "Buttons": [ {"Yes": "Yes", "Value": 1}, {"No": "No", "Value": 2} ], "NextPrevButtons": [{"Button next":"asdz"}, {"Button previous":"" }] }';
+    questionAvailableSlide.rendered.schemeContent = '{ "Title":"", "Subtitle":"", "Question":"", "Assistive text":"", "Buttons": [ {"Yes": "Yes", "Value": 1}, {"No": "No", "Value": 2}], "NextPrevButtons": [{"Button next":"asdz"}, {"Button previous":"" }] }';
 
     let sidebarItemControl2 = new SidebarItem();
     sidebarItemControl2.rendered.innerContent = await questionAvailableSlide.render();
 
     container.append(await sidebarItemControl2.render());
+
+    let multiSelectionAvailableSlide = new AvailableSlide();
+    multiSelectionAvailableSlide.rendered.title = 'Multi selection slide';
+    multiSelectionAvailableSlide.rendered.imageModifier = 'slide-wrapper__thumbnail-picture--multi';
+    multiSelectionAvailableSlide.rendered.schemeName = 'multiselect';
+    multiSelectionAvailableSlide.rendered.schemeContent = '{ "Title":"", "Subtitle":"", "Question":"", "Assistive text":"", "Buttons": [ {"Variant1": "Variant1", "Value": 1}], "NextPrevButtons": [{"Button next":"asdz"}, {"Button previous":"" }], "Multiselect": true }';
+
+    let sidebarItemControl3 = new SidebarItem();
+    sidebarItemControl3.rendered.innerContent = await multiSelectionAvailableSlide.render();
+
+    container.append(await sidebarItemControl3.render());
 
     GlobalMeta.initialize();
 });
